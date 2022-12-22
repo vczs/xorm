d52d4f0a796f478c61ff5e16fc247897d862c301
vczs <hi.vincz@gmail.com>
2022-12-23 04:28:08 +0800

    r

---
+++ curd/comm.go
@@ -0,0 +1,14 @@
+package curd
+
+// import "xorm.io/xorm"
+
+// func Comm(engine *xorm.Engine) {
+// 	// 查询或更新指定字段
+// 	engine.Cols("age", "name").Update(&user)
+// 	// 查询或更新所有字段
+// 	engine.AllCols().ID(1).Update(&user)
+// 	// 执行SQL
+// 	engine.SQL()
+// 	// 添加、查询、更新时排除指定字段  （添加时排出的字段给默认值）
+// 	engine.Omit("age, gender").Insert(&user)
+// }
---
+++ curd/read.go
@@ -0,0 +1,214 @@
+package curd
+
+import (
+	"fmt"
+	"xorm/model"
+	"xorm/vlog"
+
+	"xorm.io/xorm"
+)
+
+func R(engine *xorm.Engine) {
+	// 查询一条数据
+	getData(engine)
+
+	// 查询所有数据
+	findData(engine)
+
+	// 条件查询
+	condData(engine)
+
+	// 其他查询
+	otherData(engine)
+}
+
+// 查询一条数据 Get方法
+func getData(engine *xorm.Engine) {
+	// 根据id获取单条数据
+	user1 := new(model.User)
+	has, _ := engine.ID(102).Get(user1)
+	if !has {
+		fmt.Printf("id等于%d的数据不存在!\n", 102)
+		return
+	}
+	fmt.Printf("查询到id等于%d的数据:%v\n", 102, *user1)
+
+	// 根据结构体中已有的非空数据来获得单条数据
+	user2 := model.User{Account: "vczs002"}
+	has, _ = engine.Get(&user2)
+	if !has {
+		fmt.Printf("account等于%s的数据不存在!\n", "vczs002")
+		return
+	}
+	fmt.Printf("查询到account等于%s的数据:%v\n", "vczs002", user2)
+
+	// 根据Where来获得单条数据
+	user3 := model.User{}
+	has, _ = engine.Where("account=?", "vczs003").Get(&user3)
+	if !has {
+		fmt.Printf("account等于%s的数据不存在!\n", "vczs003")
+		return
+	}
+	fmt.Printf("查询到account等于%s的数据:%v\n", "vczs003", user3)
+}
+
+// 查询所有数据 Find方法
+func findData(engine *xorm.Engine) {
+	// Slice接收数据
+	sUsers := make([]model.User, 0)
+	err := engine.Find(&sUsers)
+	if err != nil {
+		vlog.Vlog("Slice接收数据 查询失败:", err)
+		return
+	}
+	fmt.Println("Slice接收数据:", sUsers)
+
+	// Map接收数据
+	// map的key必须为int类型,key为数据主键id(对于复合主键无法使用这种方式)
+	mUsers := make(map[int64]*model.User)
+	err = engine.Find(&mUsers)
+	if err != nil {
+		vlog.Vlog("Map接收数据 查询失败:", err)
+		return
+	}
+	// 因为mUsers的value为User的指针 所以要遍历mUsers获取每个value指向的值
+	for k, v := range mUsers {
+		fmt.Println(k, *v)
+	}
+
+	// Where获取符合要求的数据
+	wUsers := make([]model.User, 0)
+	err = engine.Where("user_age > ? or address = ?", 16, "CN").Limit(10, 0).Find(&wUsers)
+	if err != nil {
+		vlog.Vlog("Where获取符合要求的数据 查询失败:", err)
+		return
+	}
+	fmt.Println("Where获取符合要求的数据:", wUsers)
+}
+
+// 条件查询
+func condData(engine *xorm.Engine) {
+	// Where And 组合条件
+	users1 := make([]model.User, 0)
+	err := engine.Where("user_age>?", 16).And("address=?", "CN").Find(&users1)
+	if err != nil {
+		vlog.Vlog("Where And 组合条件查询失败", err)
+		return
+	}
+	fmt.Printf("Where And 组合条件查询的数据:%v\n", users1)
+
+	// 指定字段名正序排序（可以组合）
+	users2 := make([]model.User, 0)
+	err = engine.Asc("name").And("address=?", "US").Find(&users2)
+	if err != nil {
+		vlog.Vlog("指定字段名正序排序查询失败", err)
+		return
+	}
+	fmt.Printf("指定字段名正序排序查询的数据:%v\n", users2)
+
+	// 指定字段名逆序排序（可以组合）
+	users3 := make([]model.User, 0)
+	err = engine.Desc("name").Asc("user_age").Find(&users3)
+	if err != nil {
+		vlog.Vlog("指定字段名逆序排序查询失败", err)
+		return
+	}
+	fmt.Printf("指定字段名逆序排序查询的数据:%v\n", users3)
+
+	// 查询指定字段
+	user1 := new(model.User)
+	has, _ := engine.Cols("name", "account", "email").Get(user1)
+	if !has {
+		fmt.Println("查询指定字段的数据不存在!")
+		return
+	}
+	fmt.Printf("查询指定字段的数据:%v\n", *user1)
+
+	// 查询并去重指定字段
+	users4 := make([]model.User, 0)
+	engine.Distinct("name", "gender").Find(&users4)
+	if err != nil {
+		vlog.Vlog("查询并去重指定字段查询失败", err)
+		return
+	}
+	fmt.Printf("查询并去重指定字段查询的数据:%v\n", users4)
+
+	// 分页查询
+	users5 := make([]model.User, 0)
+	// Limit(number, start) 第一个参数为条数，第二个参数表示开始位置（如果不传则为0）
+	// Top(int): Limit(number, 0)
+	engine.Limit(3, 0).Find(&users5)
+	if err != nil {
+		vlog.Vlog("分页查询查询失败", err)
+		return
+	}
+	fmt.Printf("分页查询查询的数据:%v\n", users5)
+
+	// 统计数量
+	total, err := engine.Count(&model.User{Name: "vcz02"})
+	// engine.Where("user_age >?", 17).Count(new(model.User))
+	if err != nil {
+		vlog.Vlog("统计数量 查询失败:", err)
+		return
+	}
+	fmt.Println("统计数量 user_age大于17岁的数据有:", total)
+
+	// 查询数据是否存在
+	has, _ = engine.Where("name = ?", "vcz03").Exist(&model.User{})
+	// 与Get()方法相比 Exist()只判断数据是否存在不用将查询结果解析到结构体中 所以Exist的执行效率要比Get更高
+	// engine.Exist(&model.User{name: "vcz03"})
+	// engine.SQL("select * from xorm_user where name = ?", "vcz03").Exist()
+	// engine.Table(&model.User{}).Where("name = ?", "vcz03").Exist()
+	if !has {
+		fmt.Println("数据不存在!")
+		return
+	}
+	fmt.Println("数据存在")
+}
+
+// 其他查询
+func otherData(engine *xorm.Engine) {
+	// 查询单个字段
+	var strs []string
+	err := engine.Table(&model.User{}).Cols("account").Find(&strs)
+	if err != nil {
+		vlog.Vlog("查询单个字段 查询失败:", err)
+		return
+	}
+	fmt.Println("查询单个字段", strs)
+
+	// Iterate方法提供逐条执行查询到的记录的方法，他所能使用的条件和Find方法完全相同
+	engine.Where("name >? or address=?", 17, "CN").Iterate(new(model.User), func(i int, bean interface{}) error {
+		user := bean.(*model.User)
+		fmt.Printf("第%d条数据:%v\n", i, *user)
+		return nil
+	})
+
+	// Rows方法和Iterate方法类似，提供逐条执行查询到的记录的方法，不过Rows更加灵活好用。
+	rows, err := engine.Rows(&model.User{Address: "US"})
+	if err != nil {
+		vlog.Vlog("Rows方法查询失败", err)
+	}
+	defer rows.Close()
+	bean := new(model.User)
+	for rows.Next() {
+		rows.Scan(bean)
+		fmt.Println(*bean)
+	}
+
+	// SumInt求某个字段的和,返回int64 (Sum返回float64)
+	total, err := engine.Where("id >?", 102).SumInt(new(model.User), "user_age")
+	if err != nil {
+		vlog.Vlog("SumInt查询失败", err)
+	}
+	fmt.Printf("user_age的和: %d\n", total)
+
+	// SumsInt求某几个字段的和,返回int64的Slice (Sums返回float64的Slice)
+	totals, err := engine.Where("id < ?", 103).SumsInt(new(model.User), "user_age", "id")
+	if err != nil {
+		vlog.Vlog("SumsInt查询失败", err)
+	}
+	for k, v := range totals {
+		fmt.Printf("第%d个字段和: %d\n", k, v)
+	}
+}
